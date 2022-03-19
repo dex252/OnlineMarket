194e3935340596c42dbdd97930565553114fdbce
Slava <dex252rus@gmail.com>
2022-03-19 19:55:21 +1000

    Бургер меню - анимация клика

--- Scripts/Shared/common.js
+++ Scripts/Shared/common.js
@@ -6,3 +6,5 @@ import 'jquery-validation-unobtrusive';
 
 import 'styles/site.css';
 import 'libs/bootstrap/dist/css/bootstrap.css';
+
+import './header-burger-menu';
---
+++ Scripts/Shared/header-burger-menu.js
@@ -0,0 +1,16 @@
+﻿const BURGER_MENU_BUTTON = '#burger-button-catalog';
+const BURGER_MENU = '#header-burger-menu';
+$(async () => {
+    let burgerMenu = $(BURGER_MENU_BUTTON);
+    let overlay = $(BURGER_MENU);
+    burgerMenu.on('click', () => {
+        if(burgerMenu.hasClass("change")){
+            burgerMenu.removeClass("change");
+        } else{
+            burgerMenu.addClass("change");
+        }
+
+        //TODO: разгребсти кучу
+        overlay.toggle("overlay");
+    });
+});
